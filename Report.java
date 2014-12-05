006f9fd4e051b1a278b46ca3c164409a09850cf1
lakerfan94 <mattgarcia94@sbcglobal.net>
2014-12-04 22:22:31 -0800

    Update Report.java

--- Report.java
+++ Report.java
@@ -1,20 +1,26 @@
-package hw2;
+package project;
+
+
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Report {
 
 	//Print most points
 	//Print best cell config 
 	//Prefromance Time
-	File file = new File("report.txt"); 
+	File file; 
+
 	
 	
     public Report(File file) throws IOException {
         // TODO Auto-generated constructor stub
+    	this.file=file;
     	if(!file.exists()){
 			file.createNewFile();
 		}
@@ -25,20 +31,26 @@ public class Report {
 
     }
 
-    public void write(Road mostpoints, CellNetwork bestCellconfig, long startT, long endT) throws IOException {
- 
-    	long totalT = endT - startT;
-    	FileWriter fw = new FileWriter(file);
-    	BufferedWriter bw = new BufferedWriter(fw);
-		bw.write("Group: Andrew and Nikko" + "\n");
+    public void write(Collection<CellNetwork> deletedConfigurations, int initialCount, int amountDeleted) throws IOException {
+		FileWriter fw = new FileWriter(file);
+		BufferedWriter bw = new BufferedWriter(fw);
+		int count= deletedConfigurations.size();
+
+		bw.write("Deleted Configurations: ");
 		bw.newLine();
-		bw.write(mostpoints.toString());
+		for(CellNetwork cn : deletedConfigurations){
+			bw.write(cn.toString());
+			
+		}
+		bw.write("Total initial configurations: " + initialCount);
 		bw.newLine();
-		bw.write("Best Cell Configuration: " + "\n" + bestCellconfig + "\n");
+		bw.write("Amount of deleted configurations: " + count);
 		bw.newLine();
-		bw.write("Performance time: " + totalT + "ms");
-		bw.newLine();
+		bw.write("Final amount of configurations: " + (initialCount - count));
 		bw.flush();
 		bw.close();
-		}
+
+		
+
+	}
     }
